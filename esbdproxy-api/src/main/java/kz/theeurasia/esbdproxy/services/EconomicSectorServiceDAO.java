03bcfaedcb88c1f4e8ad95f017744913ba1f4fae
Vadim.Isaev <vadim.o.isaev@gmail.com>
2016-01-30 17:12:13 +0600

    DAO для EconomicsSectorDict

--- esbdproxy-api/src/main/java/kz/theeurasia/esbdproxy/services/EconomicSectorServiceDAO.java
+++ esbdproxy-api/src/main/java/kz/theeurasia/esbdproxy/services/EconomicSectorServiceDAO.java
@@ -1,6 +1,9 @@
 package kz.theeurasia.esbdproxy.services;
 
+import java.util.List;
+
 import kz.theeurasia.esbdproxy.domain.dict.EconomicSectorDict;
 
 public interface EconomicSectorServiceDAO extends DictServiceDAO<EconomicSectorDict, Long> {
+    List<EconomicSectorDict> getAll();
 }
---
+++ esbdproxy-ejbimpl/src/main/java/kz/theeurasia/esbdproxy/services/ejbimpl/dict/EconomicSectorDictServiceWS.java
@@ -0,0 +1,38 @@
+package kz.theeurasia.esbdproxy.services.ejbimpl.dict;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.Singleton;
+
+import kz.theeurasia.esbdproxy.domain.dict.EconomicSectorDict;
+import kz.theeurasia.esbdproxy.services.EconomicSectorServiceDAO;
+import kz.theeurasia.esbdproxy.services.NotFound;
+
+@Singleton
+public class EconomicSectorDictServiceWS implements EconomicSectorServiceDAO {
+
+    private List<EconomicSectorDict> all;
+
+    @PostConstruct
+    protected void init() {
+	all = new ArrayList<>();
+	for (EconomicSectorDict cd : EconomicSectorDict.values())
+	    all.add(cd);
+    }
+
+    @Override
+    public EconomicSectorDict getById(Long id) throws NotFound {
+	EconomicSectorDict result = EconomicSectorDict.forId(id);
+	if (result == null)
+	    throw new NotFound("Not found with ID = '" + id + "'");
+	return result;
+    }
+
+    @Override
+    public List<EconomicSectorDict> getAll() {
+	return all;
+    }
+
+}

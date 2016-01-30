5c2d542c90a41d3a03a5dabdb8a3c9542dd056ae
Vadim.Isaev <vadim.o.isaev@gmail.com>
2016-01-31 00:22:02 +0600

    Исправлена ошибка справочника CountryDict когда некотороые элементы имели одинаковый ID = -1. Вылетали тесткесы

--- esbdproxy-ejbimpl/src/test/java/utils/GetItems.java
+++ esbdproxy-ejbimpl/src/test/java/utils/GetItems.java
@@ -1,3 +1,5 @@
+package utils;
+
 import java.util.Iterator;
 
 import kz.theeurasia.asb.esbd.jaxws.ArrayOfItem;
---
+++ esbdproxy-ejbimpl/src/test/java/utils/SortCountries.java
@@ -0,0 +1,39 @@
+package utils;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import kz.theeurasia.esbdproxy.domain.dict.CountryDict;
+
+public class SortCountries {
+    public static void main(String[] args) {
+	SortCountries s = new SortCountries();
+	s.sortCountries();
+    }
+
+    public void sortCountries() {
+	Set<String> codes = new HashSet<>();
+	for (CountryDict cd : CountryDict.values())
+	    if (cd != CountryDict.UNSPECIFIED)
+		codes.add(cd.name());
+	String[] codesAry = codes.toArray(new String[0]);
+	Arrays.sort(codesAry);
+
+	for (String code : codesAry) {
+	    CountryDict cd = CountryDict.valueOf(code);
+	    StringBuffer sb = new StringBuffer();
+	    sb.append(String.format("%1$s(%2$s, \"%3$s\", \"%4$s\"", cd.name(), cd.getIsoCode(), cd.getRusname(),
+		    cd.getEngname()));
+	    long[] ids = cd.getIds();
+	    Arrays.sort(ids);
+	    for (long id : ids) {
+		sb.append("," + id);
+	    }
+	    sb.append(String.format("), // %1$s", cd.getRusname()));
+	    System.out.println(sb.toString());
+	}
+	System.out.println("UNSPECIFIED(0,\"\",\"\",0);");
+
+    }
+}

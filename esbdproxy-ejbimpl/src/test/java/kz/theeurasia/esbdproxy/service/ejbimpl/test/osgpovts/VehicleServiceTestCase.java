ba2f75b983215dee3356ba81d2cdcf027e57d38b
Vadim.Isaev <vadim.o.isaev@gmail.com>
2016-01-31 21:55:31 +0600

    Тест-кейс для транспортного средства

---
+++ esbdproxy-ejbimpl/src/test/java/kz/theeurasia/esbdproxy/service/ejbimpl/test/osgpovts/VehicleServiceTestCase.java
@@ -0,0 +1,43 @@
+package kz.theeurasia.esbdproxy.service.ejbimpl.test.osgpovts;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
+
+import javax.naming.NamingException;
+
+import org.junit.Test;
+
+import kz.theeurasia.esbdproxy.domain.dict.osgpovts.VehicleAgeClassDict;
+import kz.theeurasia.esbdproxy.domain.entities.osgpovts.VehicleEntity;
+import kz.theeurasia.esbdproxy.service.ejbimpl.test.GeneralServiceTestCase;
+import kz.theeurasia.esbdproxy.services.NotFound;
+import kz.theeurasia.esbdproxy.services.VehicleServiceDAO;
+import kz.theeurasia.esbdproxy.services.osgpovts.VehicleAgeClassServiceDAO;
+
+public class VehicleServiceTestCase extends GeneralServiceTestCase {
+
+    private static final long INVALID_ID = -99999l;
+    private static final String VALID_VIN_CODE = "JN1TANS51U0303376";
+    private static final String INVALID_VIN_CODE = "QASDA";
+
+    @Test
+    public void testGetByByVIN() throws NamingException {
+	VehicleServiceDAO service = getVehicleServiceEntityWS();
+	try {
+	    VehicleEntity entity = service.getByVINCode(VALID_VIN_CODE);
+	    assertThat(entity, not(nullValue()));
+	    assertThat(entity.getVinCode(), is(VALID_VIN_CODE));
+	    System.out.println(entity.getId());
+	} catch (NotFound e) {
+	    fail(e.getMessage());
+
+	}
+    }
+
+    @Test(expected = NotFound.class)
+    public void testGetByByVIN_NotFound() throws NamingException, NotFound {
+	VehicleServiceDAO service = getVehicleServiceEntityWS();
+	service.getByVINCode(INVALID_VIN_CODE);
+    }
+
+}

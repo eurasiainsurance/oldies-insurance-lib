68f26921320ef21a42fadd27526aabb061435ecc
Vadim.Isaev <vadim.o.isaev@gmail.com>
2016-02-12 09:59:40 +0600

    1. Реализован алогоритм проверки валидности email и SiteURL в соответствующих валидаторах

--- webapp/src/main/java/kz/theeurasia/policy/validator/ValidURLConstraintValidator.java
+++ webapp/src/main/java/kz/theeurasia/policy/validator/ValidURLConstraintValidator.java
@@ -1,20 +1,26 @@
 package kz.theeurasia.policy.validator;
 
+import java.util.regex.Pattern;
+
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
 public class ValidURLConstraintValidator implements ConstraintValidator<ValidURL, String> {
 
+    private static final String URL_PATTERN = "^(?:([^:/?#]+):)?(?://([^/?#]*))?([^?#]*)(?:\\?([^#]*))?(?:#(.*))?$";
+
+    private Pattern pattern;
+
     @Override
     public void initialize(ValidURL a) {
+	pattern = Pattern.compile(URL_PATTERN);
     }
 
     @Override
     public boolean isValid(String value, ConstraintValidatorContext cvc) {
 	if (value == null)
 	    return true;
-	// TODO реализовать проверку адреса сайта
-	return true;
+	return pattern.matcher(value.toString()).matches();
     }
 
 }

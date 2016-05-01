1eb3f3d6d2044a5a9f365d6ae2d41b7ac064fcac
Vadim.Isaev <vadim.o.isaev@gmail.com>
2016-05-01 19:53:42 +0600

    InsuranceClassTypeDict is now independent (w/o ESBD IDs)

--- elements/src/main/java/kz/theeurasia/esbdproxy/domain/dict/osgpovts/InsuranceClassTypeDict.java
+++ elements/src/main/java/kz/theeurasia/esbdproxy/domain/dict/osgpovts/InsuranceClassTypeDict.java
@@ -1,44 +1,22 @@
 package kz.theeurasia.esbdproxy.domain.dict.osgpovts;
 
-//TODO перенести в domain 
 public enum InsuranceClassTypeDict {
 
-    CLASS_0("0"),
-    CLASS_1("1"),
-    CLASS_2("2"),
-    CLASS_3("3"),
-    CLASS_4("4"),
-    CLASS_5("5"),
-    CLASS_6("6"),
-    CLASS_7("7"),
-    CLASS_8("8"),
-    CLASS_9("9"),
-    CLASS_10("10"),
-    CLASS_11("11"),
-    CLASS_12("12"),
-    CLASS_13("13"),
-    CLASS_M("М");
+    CLASS_0,
+    CLASS_1,
+    CLASS_2,
+    CLASS_3,
+    CLASS_4,
+    CLASS_5,
+    CLASS_6,
+    CLASS_7,
+    CLASS_8,
+    CLASS_9,
+    CLASS_10,
+    CLASS_11,
+    CLASS_12,
+    CLASS_13,
+    CLASS_M;
 
-    private final String code;
-
-    InsuranceClassTypeDict(String code) {
-	this.code = code;
-    }
-
-    @Deprecated
-    public static InsuranceClassTypeDict forCode(String code) {
-	for (InsuranceClassTypeDict ict : values()) {
-	    if (ict.getCode().equals(code))
-		return ict;
-	}
-	return null;
-    }
-
-    // GENERATED
-
-    @Deprecated
-    public String getCode() {
-	return code;
-    }
-
+    public static final String BUNDLE_BASENAME = "ElementsLocalization";
 }

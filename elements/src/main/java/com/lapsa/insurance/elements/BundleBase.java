7d3b6117261cb361a694d0fcf1aaddfdeac3a140
Vadim.Isaev <vadim.o.isaev@gmail.com>
2016-05-05 12:16:24 +0600

    bundle description in faces-config.xml defined

--- elements/src/main/java/com/lapsa/insurance/elements/BundleBase.java
+++ elements/src/main/java/com/lapsa/insurance/elements/BundleBase.java
@@ -2,4 +2,5 @@ package com.lapsa.insurance.elements;
 
 public interface BundleBase {
     public static final String BUNDLE_BASENAME = "ElementsLocalization";
+    public static final String BUNDLE_VAR = "el";
 }

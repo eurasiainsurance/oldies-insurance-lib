3cc62e0179d37f7a4cde8dee56306945f2dd9975
Vadim.Isaev <vadim.o.isaev@gmail.com>
2016-05-06 18:39:09 +0600

    new domain bean added + validation

---
+++ domain/src/main/java/kz/theeurasia/policy/domain/PolicyObtainingData.java
@@ -0,0 +1,57 @@
+package kz.theeurasia.policy.domain;
+
+import java.util.Date;
+
+import com.lapsa.insurance.elements.DeliveryTimeSlot;
+import com.lapsa.insurance.elements.PolicyObtainingMethod;
+
+import kz.theeurasia.policy.validator.NotNullValue;
+
+public class PolicyObtainingData {
+
+    @NotNullValue(message = "{kz.theeurasia.policy.domain.PolicyObtainingData.method.NotNullValue.message}")
+    private PolicyObtainingMethod method;
+
+    @NotNullValue(message = "{kz.theeurasia.policy.domain.PolicyObtainingData.deliveryDate.NotNullValue.message}")
+    private Date deliveryDate;
+
+    @NotNullValue(message = "{kz.theeurasia.policy.domain.PolicyObtainingData.deliveryTime.NotNullValue.message}")
+    private DeliveryTimeSlot deliveryTime;
+
+    @NotNullValue(message = "{kz.theeurasia.policy.domain.PolicyObtainingData.deliveryAddress.NotNullValue.message}")
+    private String deliveryAddress;
+
+    // GENERATED
+
+    public PolicyObtainingMethod getMethod() {
+	return method;
+    }
+
+    public void setMethod(PolicyObtainingMethod method) {
+	this.method = method;
+    }
+
+    public Date getDeliveryDate() {
+	return deliveryDate;
+    }
+
+    public void setDeliveryDate(Date deliveryDate) {
+	this.deliveryDate = deliveryDate;
+    }
+
+    public DeliveryTimeSlot getDeliveryTime() {
+	return deliveryTime;
+    }
+
+    public void setDeliveryTime(DeliveryTimeSlot deliveryTime) {
+	this.deliveryTime = deliveryTime;
+    }
+
+    public String getDeliveryAddress() {
+	return deliveryAddress;
+    }
+
+    public void setDeliveryAddress(String deliveryAddress) {
+	this.deliveryAddress = deliveryAddress;
+    }
+}

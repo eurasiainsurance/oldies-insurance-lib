925632f0839a0a399d6fe25244fbcfe6f765dd8b
Vadim.Isaev <vadim.o.isaev@gmail.com>
2016-05-08 17:37:49 +0600

    new delivery time slot element DURING_THE_DAY

--- elements/src/main/java/com/lapsa/insurance/elements/DeliveryTimeSlot.java
+++ elements/src/main/java/com/lapsa/insurance/elements/DeliveryTimeSlot.java
@@ -1,6 +1,7 @@
 package com.lapsa.insurance.elements;
 
 public enum DeliveryTimeSlot implements BundleBase {
+    DURING_THE_DAY, // в течении дня
     BEFORE_LUNCH, // в первой половине дня (с 9:00 до 13:00)
     AFTER_LUNCH, // во второй половине дня (с 13:00 до 18:00)
     EVENING, // вечером (с 18:00 до 21:00)

518f3c8dc394124d346a7f3d8f5d7d29e2f2d4a5
Vadim.Isaev <vadim.o.isaev@gmail.com>
2016-05-08 17:21:23 +0600

    policy delivery date validator

--- domain/src/main/java/kz/theeurasia/policy/domain/PolicyObtainingData.java
+++ domain/src/main/java/kz/theeurasia/policy/domain/PolicyObtainingData.java
@@ -5,6 +5,9 @@ import java.util.Date;
 import com.lapsa.insurance.elements.DeliveryTimeSlot;
 import com.lapsa.insurance.elements.PolicyObtainingMethod;
 
+import kz.theeurasia.policy.validator.DaysAfterNow;
+import kz.theeurasia.policy.validator.DaysBeforeNow;
+import kz.theeurasia.policy.validator.Mode;
 import kz.theeurasia.policy.validator.NotEmptyString;
 import kz.theeurasia.policy.validator.NotNullValue;
 
@@ -14,6 +17,8 @@ public class PolicyObtainingData {
     private PolicyObtainingMethod method;
 
     @NotNullValue(message = "{kz.theeurasia.policy.domain.PolicyObtainingData.deliveryDate.NotNullValue.message}")
+    @DaysAfterNow(mode = Mode.MUST, value = 0, message = "{kz.theeurasia.policy.domain.PolicyObtainingData.deliveryDate.DaysAfterNow.messages}")
+    @DaysBeforeNow(mode = Mode.MUST_NOT, value = 7, message = "{kz.theeurasia.policy.domain.PolicyObtainingData.deliveryDate.DaysBeforeNow.messages}")
     private Date deliveryDate;
 
     @NotNullValue(message = "{kz.theeurasia.policy.domain.PolicyObtainingData.deliveryTime.NotNullValue.message}")

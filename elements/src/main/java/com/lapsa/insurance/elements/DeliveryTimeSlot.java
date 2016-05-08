8f2d9942d33cc41fcdb3257ac3fd3ccf7f21e98b
Vadim.Isaev <vadim.o.isaev@gmail.com>
2016-05-08 17:37:28 +0600

    texts improvements

--- elements/src/main/java/com/lapsa/insurance/elements/DeliveryTimeSlot.java
+++ elements/src/main/java/com/lapsa/insurance/elements/DeliveryTimeSlot.java
@@ -1,8 +1,9 @@
 package com.lapsa.insurance.elements;
 
 public enum DeliveryTimeSlot implements BundleBase {
-    BEFORE_LUNCH, // с 9:00 до 13:00
-    AFTER_LUNCH, // с 13:00 до 18:00
-    EVENING, // с 18:00 до 21:00
+    BEFORE_LUNCH, // в первой половине дня (с 9:00 до 13:00)
+    AFTER_LUNCH, // во второй половине дня (с 13:00 до 18:00)
+    EVENING, // вечером (с 18:00 до 21:00)
+    //
     ;
 }

06f449b68dc98c77fcc3991b0d16ee68758f338f
Vadim.Isaev <vadim.o.isaev@gmail.com>
2016-05-18 17:47:38 +0600

    new DAO methods

--- persistence/src/main/java/com/lapsa/insurance/persistence/dao/PolicyExpressOrderDAO.java
+++ persistence/src/main/java/com/lapsa/insurance/persistence/dao/PolicyExpressOrderDAO.java
@@ -1,10 +1,22 @@
 package com.lapsa.insurance.persistence.dao;
 
-import com.lapsa.insurance.persistence.entities.PolicyExpressOrder;
+import java.util.List;
+
+import com.lapsa.insurance.crm.RequestStatus;
+import com.lapsa.insurance.domain.PolicyExpressOrder;
 
 public interface PolicyExpressOrderDAO {
 
     PolicyExpressOrder findById(int id);
 
     PolicyExpressOrder save(PolicyExpressOrder policyExpressOrder);
+
+    void save(List<PolicyExpressOrder> policyExpressOrder);
+
+    void save(PolicyExpressOrder... policyExpressOrder);
+
+    List<PolicyExpressOrder> findByStatus(RequestStatus status);
+
+    List<PolicyExpressOrder> findAllOpen();
+
 }

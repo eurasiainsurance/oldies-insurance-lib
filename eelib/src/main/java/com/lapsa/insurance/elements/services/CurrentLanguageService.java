4b0b0fbc32d22c7913cc8135227953954d6867a0
Vadim.Isaev <vadim.o.isaev@gmail.com>
2016-05-25 19:21:27 +0600

    current language stored in session scope

--- eelib/src/main/java/com/lapsa/insurance/elements/services/CurrentLanguageService.java
+++ eelib/src/main/java/com/lapsa/insurance/elements/services/CurrentLanguageService.java
@@ -1,8 +1,11 @@
 package com.lapsa.insurance.elements.services;
 
+import java.util.Locale;
+
 import com.lapsa.localization.LocalizationLanguage;
 
 public interface CurrentLanguageService {
     LocalizationLanguage getLanguage();
     void setLanguage(LocalizationLanguage language);
+    Locale getLocale();
 }
--- eelib/src/main/java/com/lapsa/insurance/elements/services/beans/DefaultCurrentLanguageService.java
+++ eelib/src/main/java/com/lapsa/insurance/elements/services/beans/DefaultCurrentLanguageService.java
@@ -1,8 +1,10 @@
 package com.lapsa.insurance.elements.services.beans;
 
+import java.io.Serializable;
 import java.util.Locale;
 
-import javax.enterprise.context.ApplicationScoped;
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.SessionScoped;
 import javax.faces.context.FacesContext;
 import javax.inject.Named;
 
@@ -10,19 +12,44 @@ import com.lapsa.insurance.elements.services.CurrentLanguageService;
 import com.lapsa.localization.LocalizationLanguage;
 
 @Named("currentLanguageService")
-@ApplicationScoped
-public class DefaultCurrentLanguageService implements CurrentLanguageService {
+@SessionScoped
+public class DefaultCurrentLanguageService implements CurrentLanguageService, Serializable {
+
+    private static final long serialVersionUID = -1456158779863948288L;
+
+    private LocalizationLanguage language;
+
+    @PostConstruct
+    public void init() {
+	// try to set language defined in user agent
+	language = getByLocale(FacesContext.getCurrentInstance().getExternalContext().getRequestLocale());
+	// if fails trying to set language defined in application as
+	// default
+	if (language == null)
+	    language = getByLocale(FacesContext.getCurrentInstance().getApplication().getDefaultLocale());
+	// if fails seting hardcoded language - RUSSIAN
+	if (language == null)
+	    language = LocalizationLanguage.RUSSIAN;
+    }
+
+    @Override
+    public Locale getLocale() {
+	return Locale.forLanguageTag(language.getTag());
+    }
 
     @Override
     public LocalizationLanguage getLanguage() {
-	Locale locale = FacesContext.getCurrentInstance().getViewRoot().getLocale();
-	LocalizationLanguage ret = LocalizationLanguage.byTag(locale.getLanguage());
-	return ret;
+	return language;
     }
 
     @Override
     public void setLanguage(LocalizationLanguage language) {
+	this.language = language;
 	Locale locale = Locale.forLanguageTag(language.getTag());
 	FacesContext.getCurrentInstance().getViewRoot().setLocale(locale);
+    }
+
+    private LocalizationLanguage getByLocale(Locale locale) {
+	return LocalizationLanguage.byTag(locale.getLanguage());
     }
 }

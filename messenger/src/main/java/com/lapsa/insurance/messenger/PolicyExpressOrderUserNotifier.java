7bdb6cff150aaefcfe035b08894fe2347751da95
Vadim.Isaev <vadim.o.isaev@gmail.com>
2016-05-26 17:10:59 +0600

    simple api

--- messenger/src/main/java/com/lapsa/insurance/messenger/PolicyExpressOrderUserNotifier.java
+++ messenger/src/main/java/com/lapsa/insurance/messenger/PolicyExpressOrderUserNotifier.java
@@ -1,10 +1,7 @@
 package com.lapsa.insurance.messenger;
 
 import com.lapsa.insurance.domain.PolicyExpressOrder;
-import com.lapsa.localization.LocalizationLanguage;
 
 public interface PolicyExpressOrderUserNotifier {
-    void notifyUser(PolicyExpressOrder policyExpressOrder, LocalizationLanguage language);
-
-    void enqueueNotifyUser(PolicyExpressOrder policyExpressOrder, LocalizationLanguage language);
+    void notifyUser(PolicyExpressOrder policyExpressOrder);
 }

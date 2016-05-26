68792b6240e6b9def2302967c6b086eb69d86c0c
Vadim.Isaev <vadim.o.isaev@gmail.com>
2016-05-26 18:03:26 +0600

    mass notify

--- messenger/src/main/java/com/lapsa/insurance/messenger/PolicyExpressOrderUserNotifier.java
+++ messenger/src/main/java/com/lapsa/insurance/messenger/PolicyExpressOrderUserNotifier.java
@@ -1,7 +1,10 @@
 package com.lapsa.insurance.messenger;
 
+import java.util.List;
+
 import com.lapsa.insurance.domain.PolicyExpressOrder;
 
 public interface PolicyExpressOrderUserNotifier {
     void notifyUser(PolicyExpressOrder policyExpressOrder);
+    void notifyUser(List<PolicyExpressOrder> policyExpressOrder);
 }

200dbae216b4ec5d2189fde69b22d7cc4c811441
Vadim.Isaev <vadim.o.isaev@gmail.com>
2016-05-26 18:02:57 +0600

    mass save entities

--- persistence/src/main/java/com/lapsa/insurance/persistence/dao/DAO.java
+++ persistence/src/main/java/com/lapsa/insurance/persistence/dao/DAO.java
@@ -1,11 +1,18 @@
 package com.lapsa.insurance.persistence.dao;
 
-public interface DAO<T,I> {
+import java.util.Collection;
+import java.util.List;
+
+public interface DAO<T, I> {
     T findById(I id);
 
     T save(T entity);
-    
+
+    void saveAll(List<T> entities);
+
     void enqueueSave(T entity);
+
+    void enqueueSaveAll(List<T> entities);
 
     void flushQueued();
 }

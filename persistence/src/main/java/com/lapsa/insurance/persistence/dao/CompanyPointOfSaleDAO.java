15dd06747e390e022dc904037c90996d0e63fbfe
Vadim.Isaev <vadim.o.isaev@gmail.com>
2016-06-01 17:12:52 +0600

    CompanyPointOfSaleDAO api

---
+++ persistence/src/main/java/com/lapsa/insurance/persistence/dao/CompanyPointOfSaleDAO.java
@@ -0,0 +1,22 @@
+package com.lapsa.insurance.persistence.dao;
+
+import java.util.List;
+
+import com.lapsa.insurance.domain.CompanyPointOfSale;
+import com.lapsa.kz.country.KZCity;
+
+public interface CompanyPointOfSaleDAO extends DAO<CompanyPointOfSale, Integer> {
+
+    List<CompanyPointOfSale> findWithPickupAvailable();
+
+    List<CompanyPointOfSale> findWithPickupAvailable(KZCity pickupCity);
+
+    List<KZCity> findCitiesWithPickupAvailable();
+
+    List<CompanyPointOfSale> findWithDeliveryAvailable();
+
+    List<CompanyPointOfSale> findWithDeliveryAvailable(KZCity deliveryCity);
+
+    List<KZCity> findCitiesWithDeliveryAvailable();
+
+}

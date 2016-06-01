68dbf68cf5d54f0732e9f7cb37bb91db11aaa756
Vadim.Isaev <vadim.o.isaev@gmail.com>
2016-06-01 18:27:26 +0600

    phoneTypeService

---
+++ eelib/src/main/java/com/lapsa/insurance/elements/services/beans/DefaultPhoneTypeService.java
@@ -0,0 +1,28 @@
+package com.lapsa.insurance.elements.services.beans;
+
+import java.util.List;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Named;
+
+import com.lapsa.insurance.elements.services.PhoneTypeService;
+import com.lapsa.phone.PhoneType;
+
+@Named("phoneTypeService")
+@ApplicationScoped
+public class DefaultPhoneTypeService extends EnumService<PhoneType> implements PhoneTypeService {
+
+    public List<PhoneType> getAllItems() {
+	return CollectionUtils.toList(PhoneType.values());
+    }
+
+    @Override
+    protected String getMessageBundleBase() {
+	return PhoneType.BUNDLE_BASENAME;
+    }
+
+    @Override
+    protected String getMessageBundleName() {
+	return PhoneType.BUNDLE_VAR;
+    }
+}

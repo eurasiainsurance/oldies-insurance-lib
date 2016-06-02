20a7f13ff57067c6c9b50e4ce0ee8f10ef7b4a46
Vadim.Isaev <vadim.o.isaev@gmail.com>
2016-06-02 15:32:10 +0600

    KZTypeOfSettlement new service

---
+++ eelib/src/main/java/com/lapsa/insurance/elements/services/KZTypeOfSettlementService.java
@@ -0,0 +1,7 @@
+
+package com.lapsa.insurance.elements.services;
+
+import com.lapsa.kz.country.KZTypeOfSettlement;
+
+public interface KZTypeOfSettlementService extends ItemService<KZTypeOfSettlement> {
+}

9088ed9f6ba88fa0424964d9a617550fa23387bf
Vadim.Isaev <vadim.o.isaev@gmail.com>
2016-06-02 16:06:52 +0600

    format

--- eelib/src/main/java/com/lapsa/insurance/elements/services/beans/DefaultLocalizationLanguageService.java
+++ eelib/src/main/java/com/lapsa/insurance/elements/services/beans/DefaultLocalizationLanguageService.java
@@ -16,34 +16,34 @@ import com.lapsa.localization.LocalizationLanguage;
 @Named("localizationLanguageService")
 @ApplicationScoped
 public class DefaultLocalizationLanguageService extends EnumService<LocalizationLanguage>
-		implements LocalizationLanguageService {
+	implements LocalizationLanguageService {
 
-	@Override
-	public List<LocalizationLanguage> getAllItems() {
-		return CollectionUtils.toList(LocalizationLanguage.values());
-	}
+    @Override
+    public List<LocalizationLanguage> getAllItems() {
+	return CollectionUtils.toList(LocalizationLanguage.values());
+    }
 
-	@Override
-	public List<LocalizationLanguage> getSelectableItems() {
-		FacesContext context = FacesContext.getCurrentInstance();
-		Iterator<Locale> i = context.getApplication().getSupportedLocales();
-		List<LocalizationLanguage> ret = new ArrayList<>();
-		while (i.hasNext()) {
-			Locale locale = i.next();
-			LocalizationLanguage l = LocalizationLanguage.byTag(locale.getLanguage());
-			if (l != null)
-				ret.add(l);
-		}
-		return ret;
+    @Override
+    public List<LocalizationLanguage> getSelectableItems() {
+	FacesContext context = FacesContext.getCurrentInstance();
+	Iterator<Locale> i = context.getApplication().getSupportedLocales();
+	List<LocalizationLanguage> ret = new ArrayList<>();
+	while (i.hasNext()) {
+	    Locale locale = i.next();
+	    LocalizationLanguage l = LocalizationLanguage.byTag(locale.getLanguage());
+	    if (l != null)
+		ret.add(l);
 	}
+	return ret;
+    }
 
-	@Override
-	protected String getMessageBundleBase() {
-		return LocalizationLanguage.BUNDLE_BASENAME;
-	}
+    @Override
+    protected String getMessageBundleBase() {
+	return LocalizationLanguage.BUNDLE_BASENAME;
+    }
 
-	@Override
-	protected String getMessageBundleVar() {
-		return LocalizationLanguage.BUNDLE_VAR;
-	}
+    @Override
+    protected String getMessageBundleVar() {
+	return LocalizationLanguage.BUNDLE_VAR;
+    }
 }

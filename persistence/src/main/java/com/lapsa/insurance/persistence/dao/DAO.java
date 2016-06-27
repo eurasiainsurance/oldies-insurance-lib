706e4fa6c7b0dc91ca6bc3a557929663135bc58b
Vadim.Isaev <vadim.o.isaev@gmail.com>
2016-06-27 17:44:29 +0600

    advanced findById

--- persistence/src/main/java/com/lapsa/insurance/persistence/dao/DAO.java
+++ persistence/src/main/java/com/lapsa/insurance/persistence/dao/DAO.java
@@ -3,9 +3,11 @@ package com.lapsa.insurance.persistence.dao;
 import java.util.List;
 
 public interface DAO<T, I> {
-    T findById(I id);
+    T findById(I id) throws EntityNotFound, PeristenceOperationFailed;
 
-    T save(T entity) throws PeristenceOperationFailed;
+    T findByIdByPassCache(I id) throws EntityNotFound, PeristenceOperationFailed;
+
+    <Z extends T> Z save(Z entity) throws PeristenceOperationFailed;
 
     void saveAll(List<T> entities) throws PeristenceOperationFailed;
 
@@ -14,4 +16,6 @@ public interface DAO<T, I> {
     void enqueueSaveAll(List<T> entities);
 
     void flushQueued();
+
+
 }
---
+++ persistence/src/main/java/com/lapsa/insurance/persistence/dao/EntityNotFound.java
@@ -0,0 +1,26 @@
+package com.lapsa.insurance.persistence.dao;
+
+public class EntityNotFound extends Exception {
+    private static final long serialVersionUID = 4444353365595112283L;
+
+    public EntityNotFound() {
+	super();
+    }
+
+    public EntityNotFound(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+	super(message, cause, enableSuppression, writableStackTrace);
+    }
+
+    public EntityNotFound(String message, Throwable cause) {
+	super(message, cause);
+    }
+
+    public EntityNotFound(String message) {
+	super(message);
+    }
+
+    public EntityNotFound(Throwable cause) {
+	super(cause);
+    }
+
+}

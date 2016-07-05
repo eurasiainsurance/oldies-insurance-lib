c170f299940ca1c4c271327a1a2bf115e97b3c4a
Vadim.Isaev <vadim.o.isaev@gmail.com>
2016-07-05 15:42:10 +0600

    NOTIFIER API change

--- mesenger/src/main/java/com/lapsa/insurance/mesenger/BaseNotifier.java
+++ mesenger/src/main/java/com/lapsa/insurance/mesenger/BaseNotifier.java
@@ -5,7 +5,7 @@ import java.util.List;
 import com.lapsa.insurance.domain.notification.Notification;
 
 public interface BaseNotifier<T extends Notification> {
-    void notify(T enitity) throws NotificationFailed;
+    void sendNotification(T enitity) throws NotificationFailed;
 
-    void notifyAll(List<T> enitities) throws NotificationFailed;
+    void sendNotification(List<T> enitities) throws NotificationFailed;
 }

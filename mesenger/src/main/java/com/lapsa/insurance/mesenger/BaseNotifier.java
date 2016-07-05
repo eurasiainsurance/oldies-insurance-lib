fa99c6873abec38adeaaf10ab9d70dcd5405b6cb
Vadim.Isaev <vadim.o.isaev@gmail.com>
2016-07-05 17:29:58 +0600

    java doc comments

--- mesenger/src/main/java/com/lapsa/insurance/mesenger/BaseNotifier.java
+++ mesenger/src/main/java/com/lapsa/insurance/mesenger/BaseNotifier.java
@@ -1,11 +1,27 @@
 package com.lapsa.insurance.mesenger;
 
-import java.util.List;
+import java.util.Collection;
 
 import com.lapsa.insurance.domain.notification.Notification;
 
 public interface BaseNotifier<T extends Notification> {
+
+    /**
+     * Send the notification entity and throws exception if somethig goes wrong
+     * 
+     * @param enitity
+     *            notifciation entity
+     * @throws NotificationFailed
+     *             exception throws
+     */
     void sendNotification(T enitity) throws NotificationFailed;
 
-    void sendNotification(List<T> enitities) throws NotificationFailed;
+    /**
+     * Methods sent a collection of Notification entities
+     * 
+     * @param enitities
+     *            a collection of Notification entities to sent
+     * @return number of notifications that was realy sent without exceptions
+     */
+    int sendMultipleNotification(Collection<T> enitities);
 }

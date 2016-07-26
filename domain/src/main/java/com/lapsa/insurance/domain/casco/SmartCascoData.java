84611bb43a2a39f7d6e621a50aeb2bcdcf087748
Vadim.Isaev <vadim.o.isaev@gmail.com>
2016-07-26 17:19:47 +0600

    casco calculation data

--- domain/src/main/java/com/lapsa/insurance/domain/casco/SmartCascoData.java
+++ domain/src/main/java/com/lapsa/insurance/domain/casco/SmartCascoData.java
@@ -1,11 +1,14 @@
 package com.lapsa.insurance.domain.casco;
 
 import com.lapsa.insurance.domain.BaseEntity;
+import com.lapsa.insurance.domain.CalculationData;
 
 public class SmartCascoData extends BaseEntity<Integer> {
     private static final long serialVersionUID = -1919280207677627360L;
     private static final int PRIME = 131;
     private static final int MULTIPLIER = PRIME;
+
+    private CalculationData calculation;
 
     @Override
     protected int getPrime() {
@@ -17,4 +20,11 @@ public class SmartCascoData extends BaseEntity<Integer> {
 	return MULTIPLIER;
     }
 
+    public CalculationData getCalculation() {
+	return calculation;
+    }
+
+    public void setCalculation(CalculationData calculation) {
+	this.calculation = calculation;
+    }
 }

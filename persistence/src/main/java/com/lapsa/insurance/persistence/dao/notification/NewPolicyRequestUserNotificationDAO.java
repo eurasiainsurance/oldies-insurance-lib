d7275eea9249ed597ac37e527c20452282907274
Vadim.Isaev <vadim.o.isaev@gmail.com>
2016-07-27 16:38:25 +0600

    format

--- persistence/src/main/java/com/lapsa/insurance/persistence/dao/notification/NewPolicyRequestUserNotificationDAO.java
+++ persistence/src/main/java/com/lapsa/insurance/persistence/dao/notification/NewPolicyRequestUserNotificationDAO.java
@@ -2,5 +2,6 @@ package com.lapsa.insurance.persistence.dao.notification;
 
 import com.lapsa.insurance.domain.notification.NewPolicyRequestUserNotification;
 
-public interface NewPolicyRequestUserNotificationDAO extends GeneralPolicyRequestNotificationDAO<NewPolicyRequestUserNotification> {
+public interface NewPolicyRequestUserNotificationDAO
+	extends GeneralPolicyRequestNotificationDAO<NewPolicyRequestUserNotification> {
 }

224fdafae266634143000cc6bdba2d61117620bf
Vadim.Isaev <vadim.o.isaev@gmail.com>
2016-08-05 17:20:51 +0600

    new module faces converters

---
+++ faces/src/main/java/com/lapsa/insurance/faces/converter/MaskedVINCodeConverter.java
@@ -0,0 +1,27 @@
+package com.lapsa.insurance.faces.converter;
+
+import java.util.Locale;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+
+@FacesConverter(value = "maskedVINCodeConverter")
+public class MaskedVINCodeConverter implements Converter {
+
+    @Override
+    public Object getAsObject(FacesContext context, UIComponent component, String value) {
+	if (value == null)
+	    return null;
+	return value.toUpperCase(Locale.ENGLISH);
+    }
+
+    @Override
+    public String getAsString(FacesContext context, UIComponent component, Object value) {
+	if (value == null)
+	    return null;
+	return value.toString();
+    }
+
+}

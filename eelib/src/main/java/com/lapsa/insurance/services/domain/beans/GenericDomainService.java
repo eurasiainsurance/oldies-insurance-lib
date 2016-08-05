1b63ed6d313c41511371972568942c6974f07cc3
Vadim.Isaev <vadim.o.isaev@gmail.com>
2016-08-05 16:50:43 +0600

    implementation of eelib moved to the own module

--- eelib/src/main/java/com/lapsa/insurance/services/domain/beans/GenericDomainService.java
+++
@@ -1,8 +0,0 @@
-package com.lapsa.insurance.services.domain.beans;
-
-import com.lapsa.insurance.domain.BaseDomain;
-import com.lapsa.insurance.services.bean.GenericItemService;
-import com.lapsa.insurance.services.domain.DomainService;
-
-public abstract class GenericDomainService<T extends BaseDomain> extends GenericItemService<T> implements DomainService<T> {
-}

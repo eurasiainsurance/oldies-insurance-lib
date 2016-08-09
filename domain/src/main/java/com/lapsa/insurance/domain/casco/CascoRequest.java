15e17143f2c80952791b86ea6985c450f4ffc5dd
Vadim.Isaev <vadim.o.isaev@gmail.com>
2016-08-09 11:01:18 +0600

    casco request notifications

--- domain/src/main/java/com/lapsa/insurance/domain/casco/CascoRequest.java
+++ domain/src/main/java/com/lapsa/insurance/domain/casco/CascoRequest.java
@@ -1,6 +1,10 @@
 package com.lapsa.insurance.domain.casco;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.lapsa.insurance.domain.InsuranceRequest;
+import com.lapsa.insurance.domain.notification.CascoRequestNotification;
 
 public class CascoRequest extends InsuranceRequest {
     private static final long serialVersionUID = -8067548829502589540L;
@@ -18,6 +22,27 @@ public class CascoRequest extends InsuranceRequest {
     }
 
     private Casco casco;
+    private List<CascoRequestNotification> notifications;
+
+    public void addNotification(CascoRequestNotification notification) {
+	if (notification == null)
+	    throw new NullPointerException("Value must not be null");
+	if (notification.getCascoRequest() != null)
+	    notification.getCascoRequest().removeNotification(notification);
+	if (notifications == null)
+	    notifications = new ArrayList<>();
+	notifications.add(notification);
+	notification.setCascoRequest(this);
+    }
+
+    public void removeNotification(CascoRequestNotification notification) {
+	if (notification == null)
+	    throw new NullPointerException("Value must not be null");
+	if (notifications == null) // nothing to remove from
+	    return;
+	notifications.remove(notification);
+	notification.setCascoRequest(null);
+    }
 
     // GENERATED
 
@@ -27,6 +52,14 @@ public class CascoRequest extends InsuranceRequest {
 
     public void setCasco(Casco casco) {
 	this.casco = casco;
+    }
+
+    public List<CascoRequestNotification> getNotifications() {
+	return notifications;
+    }
+
+    public void setNotifications(List<CascoRequestNotification> notifications) {
+	this.notifications = notifications;
     }
 
 }

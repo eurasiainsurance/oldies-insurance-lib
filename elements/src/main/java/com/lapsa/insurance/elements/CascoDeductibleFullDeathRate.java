9912e3c42e2bab64d2b8923399bb6604f00f6534
Vadim.Isaev <vadim.o.isaev@gmail.com>
2016-08-11 17:58:13 +0600

    % values

--- elements/src/main/java/com/lapsa/insurance/elements/CascoDeductibleFullDeathRate.java
+++ elements/src/main/java/com/lapsa/insurance/elements/CascoDeductibleFullDeathRate.java
@@ -3,12 +3,23 @@ package com.lapsa.insurance.elements;
 import com.lapsa.insurance.ElementsBundleBase;
 
 public enum CascoDeductibleFullDeathRate implements ElementsBundleBase {
-    PERCENT5, // 5%
-    PERCENT10, // 10%
+    PERCENT5(.05d), // 5%
+    PERCENT10(.10d), // 10%
     ;
+
+    private final double value;
 
     @Override
     public String canonicalName() {
 	return String.format("%1$s.%2$s", this.getClass().getName(), name());
     }
+
+    private CascoDeductibleFullDeathRate(double value) {
+	this.value = value;
+    }
+
+    public double getValue() {
+	return value;
+    }
+
 }
--- elements/src/main/java/com/lapsa/insurance/elements/CascoDeductiblePartialDamageRate.java
+++ elements/src/main/java/com/lapsa/insurance/elements/CascoDeductiblePartialDamageRate.java
@@ -3,17 +3,27 @@ package com.lapsa.insurance.elements;
 import com.lapsa.insurance.ElementsBundleBase;
 
 public enum CascoDeductiblePartialDamageRate implements ElementsBundleBase {
-    ZERO, // 0%
-    PERCENT0_5, // 0.5%
-    PERCENT1, // 1%
-    PERCENT2, // 2%
-    PERCENT3, // 3%
-    PERCENT5, // 5%
-    PERCENT10, // 10%
+    ZERO(.000d), // 0%
+    PERCENT0_5(.005d), // 0.5%
+    PERCENT1(.010d), // 1%
+    PERCENT2(.020d), // 2%
+    PERCENT3(.030d), // 3%
+    PERCENT5(.050d), // 5%
+    PERCENT10(.100d), // 10%
     ;
+
+    private final double value;
 
     @Override
     public String canonicalName() {
 	return String.format("%1$s.%2$s", this.getClass().getName(), name());
+    }
+
+    private CascoDeductiblePartialDamageRate(double value) {
+	this.value = value;
+    }
+
+    public double getValue() {
+	return value;
     }
 }

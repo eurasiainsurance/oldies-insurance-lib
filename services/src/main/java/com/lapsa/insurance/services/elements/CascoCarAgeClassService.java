f9074daa13f06dd993713205e6ec96c92175d4d6
Vadim.Isaev <vadim.o.isaev@gmail.com>
2016-08-12 17:21:08 +0600

    new service

---
+++ services/src/main/java/com/lapsa/insurance/services/elements/CascoCarAgeClassService.java
@@ -0,0 +1,7 @@
+package com.lapsa.insurance.services.elements;
+
+import com.lapsa.insurance.elements.CascoCarAgeClass;
+import com.lapsa.insurance.services.EnumService;
+
+public interface CascoCarAgeClassService extends EnumService<CascoCarAgeClass> {
+}

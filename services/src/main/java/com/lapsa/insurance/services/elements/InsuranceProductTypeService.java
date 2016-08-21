c931c83d291055d5eef34b0ddc551ae554713642
Vadim.Isaev <vadim.o.isaev@gmail.com>
2016-08-21 13:29:57 +0600

    InsuranceProductTypeService add

---
+++ services/src/main/java/com/lapsa/insurance/services/elements/InsuranceProductTypeService.java
@@ -0,0 +1,7 @@
+package com.lapsa.insurance.services.elements;
+
+import com.lapsa.insurance.elements.InsuranceProductType;
+import com.lapsa.insurance.services.EnumService;
+
+public interface InsuranceProductTypeService extends EnumService<InsuranceProductType> {
+}

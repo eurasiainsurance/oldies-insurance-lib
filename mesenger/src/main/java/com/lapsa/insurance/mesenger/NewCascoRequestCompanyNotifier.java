02d4135047d016842d7a5fcfa0e6ae686a558c7b
Vadim.Isaev <vadim.o.isaev@gmail.com>
2016-09-22 15:22:48 +0600

    Casco notifiers

---
+++ mesenger/src/main/java/com/lapsa/insurance/mesenger/NewCascoRequestCompanyNotifier.java
@@ -0,0 +1,6 @@
+package com.lapsa.insurance.mesenger;
+
+import com.lapsa.insurance.domain.notification.NewCascoRequestCompanyNotification;
+
+public interface NewCascoRequestCompanyNotifier extends BaseNotifier<NewCascoRequestCompanyNotification> {
+}
---
+++ mesenger/src/main/java/com/lapsa/insurance/mesenger/NewCascoRequestUserNotifier.java
@@ -0,0 +1,6 @@
+package com.lapsa.insurance.mesenger;
+
+import com.lapsa.insurance.domain.notification.NewCascoRequestUserNotification;
+
+public interface NewCascoRequestUserNotifier extends BaseNotifier<NewCascoRequestUserNotification> {
+}

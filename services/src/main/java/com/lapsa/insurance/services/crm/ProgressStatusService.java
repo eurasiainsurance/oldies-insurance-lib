dd7c84f435f3a76db8d94adba3d762b08323918d
Vadim.Isaev <vadim.o.isaev@gmail.com>
2016-11-08 15:28:14 +0600

    new services #impl eurasia36/eurasia36-crm#9, eurasia36/eurasia36-crm#13

---
+++ services/src/main/java/com/lapsa/insurance/services/crm/ProgressStatusService.java
@@ -0,0 +1,7 @@
+package com.lapsa.insurance.services.crm;
+
+import com.lapsa.insurance.crm.ProgressStatus;
+import com.lapsa.insurance.services.EnumService;
+
+public interface ProgressStatusService extends EnumService<ProgressStatus> {
+}

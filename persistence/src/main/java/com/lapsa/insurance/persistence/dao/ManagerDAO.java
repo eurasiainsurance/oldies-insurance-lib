210089e961f780ed8b56cc252f11bdb22d7057e4
Vadim.Isaev <vadim.o.isaev@gmail.com>
2016-12-07 17:56:10 +0600

    по email можно найти только одного пользователя

--- persistence/src/main/java/com/lapsa/insurance/persistence/dao/ManagerDAO.java
+++ persistence/src/main/java/com/lapsa/insurance/persistence/dao/ManagerDAO.java
@@ -1,9 +1,7 @@
 package com.lapsa.insurance.persistence.dao;
 
-import java.util.List;
-
 import com.lapsa.insurance.domain.crm.Manager;
 
 public interface ManagerDAO extends GeneralDAO<Manager, Integer> {
-    List<Manager> findByEmail(String email) throws PeristenceOperationFailed;
+    Manager findByEmail(String email) throws PeristenceOperationFailed, EntityNotFound;
 }

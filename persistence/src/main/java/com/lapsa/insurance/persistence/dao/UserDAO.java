e7b8d4ea5f43ba590177fe73f8963baaec005471
Vadim.Isaev <vadim.o.isaev@gmail.com>
2016-12-08 14:21:06 +0600

    API DAO изменение

--- persistence/src/main/java/com/lapsa/insurance/persistence/dao/UserDAO.java
+++ persistence/src/main/java/com/lapsa/insurance/persistence/dao/UserDAO.java
@@ -3,5 +3,5 @@ package com.lapsa.insurance.persistence.dao;
 import com.lapsa.insurance.domain.crm.User;
 
 public interface UserDAO extends GeneralDAO<User, Integer> {
-    User findByEmail(String email) throws PeristenceOperationFailed, EntityNotFound;
+    User findByLogin(String login) throws PeristenceOperationFailed, EntityNotFound;
 }

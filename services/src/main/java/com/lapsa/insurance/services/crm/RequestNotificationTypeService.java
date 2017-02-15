a7c07aa0e0ef9786e3f433ba97278d11e3419b0e
Vadim.Isaev <vadim.o.isaev@gmail.com>
2017-02-15 16:59:00 +0600

    RequestnotificationTypeService add

---
+++ services/src/main/java/com/lapsa/insurance/services/crm/RequestNotificationTypeService.java
@@ -0,0 +1,7 @@
+package com.lapsa.insurance.services.crm;
+
+import com.lapsa.insurance.crm.RequestNotificationType;
+import com.lapsa.insurance.services.EnumService;
+
+public interface RequestNotificationTypeService extends EnumService<RequestNotificationType> {
+}

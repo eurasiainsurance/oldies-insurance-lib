0780dc295530ab7a28335ea9f01475932bad7996
Vadim.Isaev <vadim.o.isaev@gmail.com>
2017-02-15 16:26:59 +0600

    RequestNotification added

---
+++ domain/src/main/java/com/lapsa/insurance/domain/notification/RequestNotification.java
@@ -0,0 +1,41 @@
+package com.lapsa.insurance.domain.notification;
+
+import com.lapsa.insurance.crm.RequestNotificationType;
+import com.lapsa.insurance.domain.Request;
+
+public class RequestNotification extends Notification {
+    private static final long serialVersionUID = 7362333986775221775L;
+    private static final int PRIME = 167;
+    private static final int MULTIPLIER = PRIME;
+
+    @Override
+    protected int getPrime() {
+	return PRIME;
+    }
+
+    @Override
+    protected int getMultiplier() {
+	return MULTIPLIER;
+    }
+
+    private RequestNotificationType type;
+    private Request request;
+
+    // GENERATED
+
+    public RequestNotificationType getType() {
+	return type;
+    }
+
+    public void setType(RequestNotificationType type) {
+	this.type = type;
+    }
+
+    public Request getRequest() {
+	return request;
+    }
+
+    public void setRequest(Request request) {
+	this.request = request;
+    }
+}
---
+++ elements/src/main/java/com/lapsa/insurance/crm/RequestNotificationType.java
@@ -0,0 +1,16 @@
+package com.lapsa.insurance.crm;
+
+import com.lapsa.insurance.ElementsBundleBase;
+
+public enum RequestNotificationType implements ElementsBundleBase {
+    NEW_REQUEST_USER_EMAIL_NOTIFICATION,
+    NEW_REQUEST_COMPANY_EMAIL_NOTIFICATION,
+    NEW_REQUEST_USER_PUSH_NOTIFICATION,
+    //
+    ;
+
+    @Override
+    public String canonicalName() {
+	return String.format("%1$s.%2$s", this.getClass().getName(), name());
+    }
+}

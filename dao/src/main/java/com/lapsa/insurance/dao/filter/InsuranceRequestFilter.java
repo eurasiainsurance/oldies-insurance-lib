e4ef284f955a29f7537daf38c811452f421d5a1c
Vadim.Isaev <vadim.o.isaev@gmail.com>
2017-03-22 13:02:45 +0600

    InsuranceRequestFilter spliited by two new DAO for requests

--- dao/src/main/java/com/lapsa/insurance/dao/filter/InsuranceRequestFilter.java
+++ dao/src/main/java/com/lapsa/insurance/dao/filter/InsuranceRequestFilter.java
@@ -1,28 +1,15 @@
 package com.lapsa.insurance.dao.filter;
 
-import java.util.Date;
-
+import com.lapsa.insurance.crm.InsuranceRequestType;
 import com.lapsa.insurance.crm.ObtainingStatus;
 import com.lapsa.insurance.crm.PaymentStatus;
-import com.lapsa.insurance.crm.ProgressStatus;
-import com.lapsa.insurance.crm.RequestStatus;
-import com.lapsa.insurance.crm.InsuranceRequestType;
 import com.lapsa.insurance.crm.TransactionProblem;
 import com.lapsa.insurance.crm.TransactionStatus;
-import com.lapsa.insurance.domain.crm.User;
+import com.lapsa.insurance.elements.InsuranceProductType;
 import com.lapsa.insurance.elements.ObtainingMethod;
 import com.lapsa.insurance.elements.PaymentMethod;
 
-public interface InsuranceRequestFitler {
-
-    Integer getId();
-
-    String getRequesterNameMask();
-    
-    String getRequesterIdNumberMask();
-
-    RequestStatus getRequestStatus();
-
+public interface InsuranceRequestFilter {
     InsuranceRequestType getRequestType();
 
     PaymentMethod getPaymentMethod();
@@ -33,21 +20,9 @@ public interface InsuranceRequestFitler {
 
     ObtainingStatus getObtainingStatus();
 
-    ProgressStatus getProgressStatus();
+    InsuranceProductType getInsuranceProductType();
 
     TransactionStatus getTransactionStatus();
 
     TransactionProblem getTransactionProblem();
-
-    Date getCreatedAfter();
-
-    Date getCreatedBefore();
-
-    Date getCompletedAfter();
-
-    Date getCompletedBefore();
-
-    User getAcceptedBy();
-
-    User getCompletedBy();
 }

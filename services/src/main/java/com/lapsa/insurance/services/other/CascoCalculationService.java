ba66da9962e4c87fb0de2113fe0bb63d5cd02962
Vadim.Isaev <vadim.o.isaev@gmail.com>
2017-04-07 17:51:57 +0600

    casco and policy calculator service api changed. periods removed

--- services/src/main/java/com/lapsa/insurance/services/other/CascoCalculationService.java
+++ services/src/main/java/com/lapsa/insurance/services/other/CascoCalculationService.java
@@ -1,29 +1,10 @@
 package com.lapsa.insurance.services.other;
 
-import java.time.LocalDate;
-import java.util.Calendar;
-
 import com.lapsa.insurance.domain.CalculationData;
 import com.lapsa.insurance.domain.casco.Casco;
 
 public interface CascoCalculationService {
     CalculationData calculateCasco(Casco casco) throws CalculationFailed;
 
-    CalculationData calculateCasco(Casco casco, Calendar startDate, Calendar endDate) throws CalculationFailed;
-
-    CalculationData calculateCasco(Casco casco, Calendar startDate, int days) throws CalculationFailed;
-
-    CalculationData calculateCasco(Casco casco, LocalDate startDate, LocalDate endDate) throws CalculationFailed;
-
-    CalculationData calculateCasco(Casco casco, LocalDate startDate, int days) throws CalculationFailed;
-
     void calculateCascoCost(Casco casco) throws CalculationFailed;
-
-    void calculateCascoCost(Casco casco, Calendar startDate, Calendar endDate) throws CalculationFailed;
-
-    void calculateCascoCost(Casco casco, Calendar startDate, int days) throws CalculationFailed;
-
-    void calculateCascoCost(Casco casco, LocalDate startDate, LocalDate endDate) throws CalculationFailed;
-
-    void calculateCascoCost(Casco casco, LocalDate startDate, int days) throws CalculationFailed;
 }

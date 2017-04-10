d372b83913517ec873235745a9c0e2589e750450
Vadim.Isaev <vadim.o.isaev@gmail.com>
2017-04-10 14:17:30 +0600

    remove unused method from API

--- services/src/main/java/com/lapsa/insurance/services/other/CascoCalculationService.java
+++ services/src/main/java/com/lapsa/insurance/services/other/CascoCalculationService.java
@@ -1,10 +1,7 @@
 package com.lapsa.insurance.services.other;
 
-import com.lapsa.insurance.domain.CalculationData;
 import com.lapsa.insurance.domain.casco.Casco;
 
 public interface CascoCalculationService {
-    CalculationData calculateCasco(Casco casco) throws CalculationFailed;
-
     void calculateCascoCost(Casco casco) throws CalculationFailed;
 }
--- services/src/main/java/com/lapsa/insurance/services/other/PolicyCalculationService.java
+++ services/src/main/java/com/lapsa/insurance/services/other/PolicyCalculationService.java
@@ -1,10 +1,7 @@
 package com.lapsa.insurance.services.other;
 
-import com.lapsa.insurance.domain.CalculationData;
 import com.lapsa.insurance.domain.policy.Policy;
 
 public interface PolicyCalculationService {
-    CalculationData calculate(Policy policy);
-
     void calculatePolicyCost(Policy policy);
 }

113303de596571fa8f1a2a6ebf12559e451fbf57
Vadim.Isaev <vadim.o.isaev@gmail.com>
2017-04-21 11:37:58 +0600

    троллебусы и трамваи не подходят для ОСГПОВТС для частных лиц

--- elements/src/main/java/com/lapsa/insurance/elements/VehicleClass.java
+++ elements/src/main/java/com/lapsa/insurance/elements/VehicleClass.java
@@ -9,7 +9,7 @@ public enum VehicleClass implements ElementsBundleBase {
     TRAILER(true), // Прицепы (полуприцепы)
     BUS16(true), // Автобусы до 16 пассажирских мест включительно
     BUSOVR16(true), // Автобусы свыше 16 пассажирских мест
-    TRAM(true), // Троллейбусы, трамваи
+    TRAM(false), // Троллейбусы, трамваи
     SEA(false), // Морское
     AIRCRAFT(false), // Воздушный транспорт
     RAILWAY(false), // Железнодорожный транспорт

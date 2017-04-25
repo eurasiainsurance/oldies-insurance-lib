cf21f57795e19e70de47038a72a977b5eaa9a9ba
Vadim.Isaev <vadim.o.isaev@gmail.com>
2017-04-25 17:53:44 +0600

    ValidPaymentMethod validator

---
+++ validation/src/main/java/com/lapsa/insurance/validation/validators/ValidPaymentMethodConstraintValidator.java
@@ -0,0 +1,37 @@
+package com.lapsa.insurance.validation.validators;
+
+import com.lapsa.insurance.elements.PaymentMethod;
+import com.lapsa.insurance.validation.ValidPaymentMethod;
+import com.lapsa.validation.ValidationMode;
+import com.lapsa.validation.constraints.AValidEnumValueConstraintValidator;
+
+public class ValidPaymentMethodConstraintValidator
+	extends AValidEnumValueConstraintValidator<ValidPaymentMethod, PaymentMethod> {
+
+    private PaymentMethod[] permited;
+    private PaymentMethod[] denied;
+    private ValidationMode mode;
+
+    @Override
+    public void initialize(ValidPaymentMethod a) {
+	this.permited = a.permited();
+	this.denied = a.denied();
+	this.mode = a.mode();
+    }
+
+    @Override
+    protected PaymentMethod[] getPermited() {
+	return permited;
+    }
+
+    @Override
+    protected PaymentMethod[] getDenied() {
+	return denied;
+    }
+
+    @Override
+    protected ValidationMode getMode() {
+	return mode;
+    }
+
+}

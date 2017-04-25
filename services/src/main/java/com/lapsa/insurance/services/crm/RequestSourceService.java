6f84f7b328dc3bfd9fbd316a0f3154573fcb840c
Vadim.Isaev <vadim.o.isaev@gmail.com>
2017-04-25 16:10:54 +0600

    RequestSource service API

---
+++ services/src/main/java/com/lapsa/insurance/services/crm/RequestSourceService.java
@@ -0,0 +1,7 @@
+package com.lapsa.insurance.services.crm;
+
+import com.lapsa.insurance.crm.RequestSource;
+import com.lapsa.insurance.services.EnumService;
+
+public interface RequestSourceService extends EnumService<RequestSource> {
+}

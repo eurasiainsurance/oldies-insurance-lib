f2ac11b062c128c70c80883f4c9acd4c564816be
Vadim.Isaev <vadim.o.isaev@gmail.com>
2017-04-25 16:10:30 +0600

    InsuranceRequestType validator

---
+++ validation/src/main/java/com/lapsa/insurance/validation/ValidInsuranceRequestTypeValue.java
@@ -0,0 +1,36 @@
+package com.lapsa.insurance.validation;
+
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.RetentionPolicy.*;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+
+import com.lapsa.insurance.crm.InsuranceRequestType;
+import com.lapsa.insurance.validation.validators.ValidInsuranceRequestTypeValueConstraintValidator;
+
+@Target({ METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER })
+@Retention(RUNTIME)
+@Constraint(validatedBy = ValidInsuranceRequestTypeValueConstraintValidator.class)
+public @interface ValidInsuranceRequestTypeValue {
+
+    public static enum ValidationMode {
+	DENY_IF_NOT_PERMITED, PERMIT_IF_NOT_DENIED, AUTO;
+    }
+
+    InsuranceRequestType[] denied() default {};
+
+    InsuranceRequestType[] permited() default {};
+
+    ValidationMode mode() default ValidationMode.AUTO;
+
+    String message() default "{com.lapsa.insurance.validation.ValidInsuranceRequestTypeValue.message}";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+
+}

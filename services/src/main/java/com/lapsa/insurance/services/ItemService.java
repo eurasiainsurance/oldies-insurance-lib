24c339e80a7f355b285421dcdad551fe7ce357b5
Vadim.Isaev <vadim.o.isaev@gmail.com>
2017-09-19 12:22:39 +0600

    members sorted

--- services/src/main/java/com/lapsa/insurance/services/ItemService.java
+++ services/src/main/java/com/lapsa/insurance/services/ItemService.java
@@ -15,21 +15,21 @@ public interface ItemService<T> {
 
     String displayName(T value, LocalizationLanguage lang);
 
-    String displayNameShort(T value);
-
-    String displayNameShort(T value, Locale locale);
-
-    String displayNameShort(T value, LocalizationLanguage lang);
-
     String displayNameFull(T value);
 
     String displayNameFull(T value, Locale locale);
 
     String displayNameFull(T value, LocalizationLanguage lang);
 
-    List<SelectItem> displayNamesShortSI(List<T> list);
-
     List<SelectItem> displayNamesFullSI(List<T> list);
+
+    String displayNameShort(T value);
+
+    String displayNameShort(T value, Locale locale);
+
+    String displayNameShort(T value, LocalizationLanguage lang);
+
+    List<SelectItem> displayNamesShortSI(List<T> list);
 
     List<SelectItem> displayNamesSI(List<T> list);
 

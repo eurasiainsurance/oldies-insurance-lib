fa365d1ba7162d9709d28204802afa8643ccb75b
Vadim.Isaev <vadim.o.isaev@gmail.com>
2017-09-21 16:42:37 +0600

    kz services moved

--- services/src/main/java/com/lapsa/insurance/services/kz/KZAreaService.java
+++
@@ -1,7 +0,0 @@
-package com.lapsa.insurance.services.kz;
-
-import com.lapsa.insurance.services.EnumListingNamingService;
-import com.lapsa.kz.country.KZArea;
-
-public interface KZAreaService extends EnumListingNamingService<KZArea> {
-}
--- services/src/main/java/com/lapsa/insurance/services/kz/KZTypeOfSettlementService.java
+++
@@ -1,8 +0,0 @@
-
-package com.lapsa.insurance.services.kz;
-
-import com.lapsa.insurance.services.EnumListingNamingService;
-import com.lapsa.kz.country.KZTypeOfSettlement;
-
-public interface KZTypeOfSettlementService extends EnumListingNamingService<KZTypeOfSettlement> {
-}

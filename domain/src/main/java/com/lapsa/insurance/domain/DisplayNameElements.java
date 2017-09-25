97e4ab783befccd81c4ecf58361187dce63c857f
Vadim.Isaev <vadim.o.isaev@gmail.com>
2017-09-25 15:32:02 +0600

    PostAddress localized

--- domain/src/main/java/com/lapsa/insurance/domain/DisplayNameElements.java
+++ domain/src/main/java/com/lapsa/insurance/domain/DisplayNameElements.java
@@ -11,5 +11,8 @@ public enum DisplayNameElements implements LocalizedElement {
     //
     CALCULATION_DATA,
     CALCULATION_DATA_EMPTY,
+    //
+    POST_ADDRESS,
+    POST_ADDRESS_EMPTY,
     ;
 }

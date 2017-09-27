9005406fa277747e2ad2d687c1b9cc8182234e45
Vadim.Isaev <vadim.o.isaev@gmail.com>
2017-09-27 16:58:35 +0600

    calculations moved to the own project insurance-calculation

--- services/src/main/java/com/lapsa/insurance/services/other/CascoCalculationService.java
+++
@@ -1,7 +0,0 @@
-package com.lapsa.insurance.services.other;
-
-import com.lapsa.insurance.domain.casco.Casco;
-
-public interface CascoCalculationService {
-    void calculateCascoCost(Casco casco) throws CalculationFailed;
-}
--- services/src/main/java/com/lapsa/insurance/services/other/PolicyCalculationService.java
+++
@@ -1,7 +0,0 @@
-package com.lapsa.insurance.services.other;
-
-import com.lapsa.insurance.domain.policy.Policy;
-
-public interface PolicyCalculationService {
-    void calculatePolicyCost(Policy policy) throws CalculationFailed;
-}

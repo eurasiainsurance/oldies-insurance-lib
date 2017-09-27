79090bef81f0c107a78dd0d0ebd5cfb165eb5966
Vadim.Isaev <vadim.o.isaev@gmail.com>
2017-09-27 18:36:43 +0600

    insurance-services refactored

--- services/src/main/java/com/lapsa/insurance/services/domain/CompanyContactPhoneService.java
+++
@@ -1,6 +0,0 @@
-package com.lapsa.insurance.services.domain;
-
-import com.lapsa.insurance.domain.CompanyContactPhone;
-
-public interface CompanyContactPhoneService extends DomainService<CompanyContactPhone> {
-}
--- services/src/main/java/com/lapsa/insurance/services/domain/DomainService.java
+++
@@ -1,7 +0,0 @@
-package com.lapsa.insurance.services.domain;
-
-import com.lapsa.insurance.domain.BaseDomain;
-import com.lapsa.insurance.services.ItemNamingService;
-
-public interface DomainService<T extends BaseDomain> extends ItemNamingService<T> {
-}

a7944ad4322248133a778b0513910975e4453453
Vadim.Isaev <vadim.o.isaev@gmail.com>
2017-12-25 14:34:03 +0600

    domain extracted

--- domain/src/main/java/com/lapsa/insurance/jpa/InsuranceConstants.java
+++
@@ -1,9 +0,0 @@
-package com.lapsa.insurance.jpa;
-
-public final class InsuranceConstants {
-
-    private InsuranceConstants() {
-    }
-
-    public static final String PERSISTENCE_UNIT_NAME = "insurancePU";
-}
